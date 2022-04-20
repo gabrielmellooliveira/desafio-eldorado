41f4fc1b01d9b151e80f01131e440ef8a170c78f
gabrielmellooliveira <gabrielmello3011@gmail.com>
2022-04-19 21:28:54 -0300

    Add middlewares

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth')
+
+function auth(request, response, next) {
+  const { authorization } = request.headers
+
+  if (authorization === null || authorization === undefined) {
+    return response.status(404).json({ mensagem: 'Token not found' })
+  }
+
+  const token = authorization.split(' ')[1]
+
+  try {
+    jwt.verify(token, authConfig.secreteKey, authConfig.options)
+
+    next()
+  } catch (error) {
+    return response.status(401).json({ mensagem: 'No autorize' })
+  }
+}
+
+module.exports = auth
