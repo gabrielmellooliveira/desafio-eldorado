f9d519d4461f4152020bb309485af7ae092655de
gabrielmellooliveira <gabrielmello3011@gmail.com>
2022-04-26 21:23:58 -0300

    Add testing env

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,9 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV.trim() === 'test' ? '.env.testing' : '.env'
+})
+
 const express = require('express')
+const cors = require('cors')
 
 const auth = require('./middlewares/auth')
 const LoginRoutes = require('./routes/LoginRoutes')
@@ -7,6 +12,7 @@ const { registerRoutes } = require('./routes')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/login', LoginRoutes)
