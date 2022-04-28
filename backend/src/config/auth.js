c25d3bf193110d3b16ce36194ad27950c3f0dd93
gabrielmellooliveira <gabrielmello3011@gmail.com>
2022-04-28 19:12:28 -0300

    Update configs

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-  secreteKey: '',
+  secreteKey: 'D06C45CA87F82E8E23E460BACAF12B3C13280704147AD55E3C3051DFFA25DA27',
   options: {
     expiresIn: '7d'
   }
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  database: process.env.NOME_DO_BANCO_DE_DADOS,
-  username: 'root',
-  password: 'password',
+  database: 'auto-service-5-test', //process.env.NOME_DO_BANCO_DE_DADOS,
+  username: 'admin',
+  password: 'gabrieladmin',
   dialect: 'mysql',
-  host: 'localhost',
+  host: 'mysqlserver.cv0e2a2edowr.sa-east-1.rds.amazonaws.com',
   port: 3306
 }
